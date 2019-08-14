ca647018b72c9d38345883936e86e713a2939e89
jungkyu-lee <jungkyu.lee@navercorp.com>
2019-08-14 17:31:41 +0900

    AttributeError: module 'contextlib' has no attribute 'nested'

--- image/main.py
+++ image/main.py
@@ -19,7 +19,22 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import contextlib
+
+try:
+    from contextlib import nested  # Python 2
+except ImportError:
+    from contextlib import ExitStack, contextmanager
+
+    @contextmanager
+    def nested(*contexts):
+        """
+        Reimplementation of nested in python 3.
+        """
+        with ExitStack() as stack:
+            for ctx in contexts:
+                stack.enter_context(ctx)
+            yield contexts
+
 import os
 import time
 import json
